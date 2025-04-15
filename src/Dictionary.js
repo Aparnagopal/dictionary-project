f68e24c71f155ee5d87e7afd7e37fa2fc2d6cb71
Aparnagopal <gopal_aparna@yahoo.com>
2025-04-15 22:11:51 +0530

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setkeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiurl = "https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}";
+    axios.get(apiurl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
