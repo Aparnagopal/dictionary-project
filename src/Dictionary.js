93f29b1d48838302683ca2a6968fe78c6b8d5975
Aparnagopal <gopal_aparna@yahoo.com>
2025-04-17 19:24:47 +0530

    Added Results and Meaning functions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setkeyword] = useState("");
+  let [results, setresults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setresults(response.data[0]);
   }
 
   function search(event) {
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search} onChange={handleKeywordChange}>
         <input type="search" autoFocus={true}></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
