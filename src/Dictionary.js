cda27499786cba5cf9c6c9f347c53732df0f2ec8
Aparnagopal <gopal_aparna@yahoo.com>
2025-04-30 20:49:36 +0530

    updated Photos.js Photos.css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setkeyword] = useState(props.defaultkeyword);
   let [results, setresults] = useState(null);
   let [loaded, setloaded] = useState(false);
+  let [photos, setphotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
@@ -14,18 +16,19 @@ export default function Dictionary(props) {
   }
 
   function handlePexelResponse(response) {
-    console.log(response.data[0]);
+    setphotos(response.data.photos);
   }
 
   function search() {
     let apiurl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiurl).then(handleResponse);
-    let pexelapikey = `HvPli8GySxCxNFpi0lJ4ZCbXLmeylR7d2FT18ObrFuFYsZ5K0AcIcR08`;
-    let pexelapiurl = `https://api.pexels.com/v1/search?query=${keyword}&key=${pexelapikey}`;
+    let pexelapikey =
+      "HvPli8GySxCxNFpi0lJ4ZCbXLmeylR7d2FT18ObrFuFYsZ5K0AcIcR08";
+    let pexelapiurl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     axios
       .get(pexelapiurl, {
         headers: {
-          "Access-Control-Allow-Origin": "*",
+          Authorization: `${pexelapikey}`,
         },
       })
       .then(handlePexelResponse);
@@ -60,6 +63,7 @@ export default function Dictionary(props) {
             ></input>
           </form>
           <Results results={results} />
+          <Photos photos={photos} />
         </section>
       </div>
     );
